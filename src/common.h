d811de381e3c1354f8d71f5dc979b1fe6cbb23c3
karei <eetu.kivelae@gmail.com>
2024-03-16 19:29:13 +0200

    Add `common.h` with integer type definitions

---
+++ src/common.h
@@ -0,0 +1,13 @@
+#ifndef COQUE_COMMON_H
+#define COQUE_COMMON_H
+
+#define u8 unsigned char
+#define i8 char
+#define u16 unsigned short
+#define i16 short
+#define u32 unsigned int
+#define i32 int
+#define u64 unsigned long long
+#define i64 unsigned long long
+
+#endif
